c6a552e4a77d98ecd4947e267f60985c41618e3c
lilinkun <503735093@qq.com>
2020-01-13 09:29:18 +0800

    修改的

---
+++ app/src/main/java/com/mingtai/mt/activity/WebviewActivity.java
@@ -0,0 +1,21 @@
+package com.mingtai.mt.activity;
+
+import com.mingtai.mt.R;
+import com.mingtai.mt.base.BaseActivity;
+
+/**
+ * Created by LG on 2020/1/13.
+ */
+public class WebviewActivity extends BaseActivity {
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_webview;
+    }
+
+    @Override
+    public void initEventAndData() {
+
+    }
+}
