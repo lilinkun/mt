759921fc2ed832e088c56162ac42944c3381f261
lilinkun <503735093@qq.com>
2020-01-13 10:32:37 +0800

    家中改

---
+++ app/src/main/java/com/mingtai/mt/activity/ForgetPsdActivity.java
@@ -0,0 +1,19 @@
+package com.mingtai.mt.activity;
+
+import com.mingtai.mt.R;
+import com.mingtai.mt.base.BaseActivity;
+
+public class ForgetPsdActivity extends BaseActivity {
+
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_forget_psd;
+    }
+
+    @Override
+    public void initEventAndData() {
+
+    }
+}
